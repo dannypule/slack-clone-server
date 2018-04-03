2782402cc355854cfab1d77842abf49721bdf79e
dannypule <kojikusan@gmail.com>
2018-04-03 22:38:22 +0100

    add cors

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,6 @@ module.exports = {
     "no-underscore-dangle": 0,
     quotes: [2, "single", { avoidEscape: true, allowTemplateLiterals: true }],
     semi: [2, "never"],
-    "import/first": 0,
     "import/prefer-default-export": 0,
     "import/newline-after-import": 0,
     "function-paren-newline": 0,
